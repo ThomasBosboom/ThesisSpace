d006235bc0bb7b2b07eb3de36445217d152dc4a9
ThomasBosboom <thomasbosboom@hotmail.com>
2023-12-19 20:03:54 +0100

    Update low_fidelity.py

--- simulations/src/dynamic_models/low_fidelity/integration_settings/low_fidelity.py
+++ simulations/src/dynamic_models/low_fidelity/integration_settings/low_fidelity.py
@@ -21,10 +21,11 @@ from DynamicModelBase import DynamicModelBase
 
 class LowFidelityDynamicModel(DynamicModelBase):
 
-    def __init__(self, simulation_start_epoch_MJD, propagation_time):
+    def __init__(self, simulation_start_epoch_MJD, propagation_time, custom_initial_state=None):
         super().__init__(simulation_start_epoch_MJD, propagation_time)
 
         # Get CRTBP characteristics
+        self.custom_initial_state = custom_initial_state
         self.distance_between_primaries = 3.84747963e8
         self.eccentricity = 0
         self.bodies_mass = [0,0]
@@ -68,11 +69,6 @@ class LowFidelityDynamicModel(DynamicModelBase):
         omega_w_norm = np.linalg.norm(np.dot(rsw_to_inertial_rotation_matrix[:, 2], self.rotation_rate))
         Omega = np.array([[0, omega_w_norm, 0],[-omega_w_norm, 0, 0],[0, 0, 0]])
 
-        # rotational_rates = np.cross(self.initial_cartesian_moon_state[:3], self.initial_cartesian_moon_state[3:])/np.linalg.norm(self.initial_cartesian_moon_state[:3])**2
-        # w1, w2, w3 = rotational_rates[:3]
-        # Omega = np.array([[0, -w3, w2],
-        #                   [w3, 0, -w1],
-        #                   [-w2, w1, 0]])
         time_derivative_rsw_to_inertial_rotation_matrix = -np.dot(rsw_to_inertial_rotation_matrix, Omega)
         total_rsw_to_inertial_rotation_matrix = np.block([[rsw_to_inertial_rotation_matrix, np.zeros((3,3))],
                         [time_derivative_rsw_to_inertial_rotation_matrix, rsw_to_inertial_rotation_matrix]])
@@ -84,141 +80,7 @@ class LowFidelityDynamicModel(DynamicModelBase):
         initial_state_lumio = self.initial_cartesian_moon_state + np.dot(total_rsw_to_inertial_rotation_matrix,initial_state_moon_fixed_lumio)
         self.initial_state = np.concatenate((initial_state_lpf, initial_state_lumio))
 
-
-    # def convert_cartesian_state_to_synodic(self):
-
-    #     self.set_initial_cartesian_moon_state()
-
-    #     # initial_state_LPF = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_ELO, )
-    #     state_history_moon = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_LPO, body="moon", get_epoch_in_array=True, get_full_history=True)
-    #     state_history_lumio = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_LPO, body="satellite", get_epoch_in_array=True, get_full_history=True)
-
-
-    #     # Extract time and state vectors from your data
-    #     epochs = state_history_moon[:, 0]  # Assuming the first column represents time
-    #     state_moon = state_history_moon[:, 1:]  # Assuming the remaining columns represent the state
-
-    #     state_lumio = state_history_lumio[:, 1:]
-
-
-    #     initial_states = np.empty((len(epochs),12))
-    #     state_history_erdem = validation_LUMIO.get_state_history_erdem()[:,1:]
-    #     state_history_erdem_bary = state_history_erdem
-    #     # state_history_erdem_bary = np.array([0.97533,	-0.0035687,	-0.020545,	0.12461,	-0.40584,	0.045309,	1.0974,	0.12988	,-0.070446,	0.10002,	-0.05159,	-0.19379])
-    #     # state_history_erdem_bary[0] = state_history_erdem_bary[0] - (1-self.mu)
-    #     # state_history_erdem_bary[6] = state_history_erdem_bary[6] - (1-self.mu)
-    #     for index, epoch in enumerate(epochs):
-
-    #         rsw_to_inertial_rotation_matrix = frame_conversion.rsw_to_inertial_rotation_matrix(state_history_moon[index, 1:])
-
-    #         rotational_rates = np.cross(state_moon[index, :3], state_moon[index, 3:])/np.linalg.norm(state_moon[index, :3])**2
-    #         w1, w2, w3 = rotational_rates[0], rotational_rates[1], rotational_rates[2]
-    #         Omega = np.array([[0, -w3, w2],
-    #                         [w3, 0, -w1],
-    #                         [-w2, w1, 0]])
-    #         # omega_w_norm = np.linalg.norm(np.dot(rsw_to_inertial_rotation_matrix[:, 2], self.rotation_rate))
-    #         # Omega = np.array([[0, omega_w_norm, 0],[-omega_w_norm, 0, 0],[0, 0, 0]])
-    #         time_derivative_rsw_to_inertial_rotation_matrix = np.dot(rsw_to_inertial_rotation_matrix, -Omega)
-    #         total_rsw_to_inertial_rotation_matrix = np.block([[rsw_to_inertial_rotation_matrix, np.zeros((3,3))],
-    #                     [time_derivative_rsw_to_inertial_rotation_matrix, rsw_to_inertial_rotation_matrix]])
-
-    #         # Convert barycentric CRTBP non-dimensionalized state into moon fixed frame.
-    #         # initial_state_moon_fixed = state_history_erdem_bary
-    #         epoch = index*7
-    #         state_history_erdem_bary[epoch, 0] = state_history_erdem_bary[epoch, 0] - (1-self.mu)
-    #         state_history_erdem_bary[epoch, 6] = state_history_erdem_bary[epoch, 6] - (1-self.mu)
-    #         initial_state_moon_fixed = state_history_erdem_bary[epoch]
-
-    #         # Convert initial state defined in CRTBP synodic frame to Earth-centered J2000 frame
-    #         self.lu_cr3bp = np.linalg.norm(state_history_moon[index, 1:4])
-    #         self.tu_cr3bp = 1/np.sqrt((self.gravitational_parameter_primary + self.gravitational_parameter_secondary)/self.lu_cr3bp**3)
-    #         initial_state_moon_fixed_lpf = np.concatenate((initial_state_moon_fixed[:3]*self.lu_cr3bp, initial_state_moon_fixed[3:6]*self.lu_cr3bp/self.tu_cr3bp))
-    #         initial_state_moon_fixed_lumio = np.concatenate((initial_state_moon_fixed[6:9]*self.lu_cr3bp, initial_state_moon_fixed[9:12]*self.lu_cr3bp/self.tu_cr3bp))
-    #         initial_state_lpf = state_history_moon[index, 1:] + np.dot(total_rsw_to_inertial_rotation_matrix,initial_state_moon_fixed_lpf)
-    #         initial_state_lumio = state_history_moon[index, 1:] + np.dot(total_rsw_to_inertial_rotation_matrix,initial_state_moon_fixed_lumio)
-
-    #         self.initial_state = np.concatenate((initial_state_lpf, initial_state_lumio))
-
-    #         initial_states[index]  = self.initial_state
-
-
-    #     ax = plt.figure().add_subplot(projection='3d')
-    #     plt.plot(initial_states[:,0], initial_states[:,1], initial_states[:,2])
-    #     plt.plot(initial_states[:,6], initial_states[:,7], initial_states[:,8])
-    #     plt.plot(state_history_moon[:,1], state_history_moon[:,2], state_history_moon[:,3])
-    #     plt.axis("equal")
-    #     plt.show()
-
-
-    # def convert_cartesian_state_to_synodic(self):
-
-    #     self.set_initial_cartesian_moon_state()
-
-    #     # initial_state_LPF = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_ELO, )
-    #     state_history_moon = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_LPO, body="satellite", get_epoch_in_array=True, get_full_history=True)
-    #     state_history_lpf = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_ELO, body="satellite", get_epoch_in_array=True, get_full_history=True)
-    #     state_history_lumio = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_LPO, body="moon", get_epoch_in_array=True, get_full_history=True)
-
-
-    #     # Extract time and state vectors from your data
-    #     epochs = state_history_moon[:, 0]  # Assuming the first column represents time
-    #     state = state_history_moon[:, 1:]  # Assuming the remaining columns represent the state
-
-    #     state_lpf = state_history_lpf[:, 1:]
-    #     state_lumio = state_history_lumio[:, 1:]
-
-    #     print(np.shape(state_lpf), np.shape(state_lumio))
-
-
-    #     initial_states = np.empty((len(epochs),12))
-    #     state_history_erdem = validation_LUMIO.get_state_history_erdem()[0,1:]
-    #     state_history_erdem_bary = state_history_erdem
-    #     # state_history_erdem_bary = np.array([0.97533,	-0.0035687,	-0.020545,	0.12461,	-0.40584,	0.045309,	1.0974,	0.12988	,-0.070446,	0.10002,	-0.05159,	-0.19379])
-    #     state_history_erdem_bary[0] = state_history_erdem_bary[0] - (1-self.mu)
-    #     state_history_erdem_bary[6] = state_history_erdem_bary[6] - (1-self.mu)
-    #     for index, epoch in enumerate(epochs):
-
-    #         rsw_to_inertial_rotation_matrix = frame_conversion.rsw_to_inertial_rotation_matrix(state_history_moon[index, 1:])
-
-    #         rotational_rates = np.cross(state[index, :3], state[index, 3:]) / np.linalg.norm(state[index, :3])**2
-    #         w1, w2, w3 = rotational_rates[0], rotational_rates[1], rotational_rates[2]
-    #         Omega = np.array([[0, -w3, w2], [w3, 0, -w1], [-w2, w1, 0]])
-    #         time_derivative_rsw_to_inertial_rotation_matrix = np.dot(rsw_to_inertial_rotation_matrix, -Omega)
-    #         total_rsw_to_inertial_rotation_matrix = np.block([[rsw_to_inertial_rotation_matrix, np.zeros((3,3))],
-    #                     [time_derivative_rsw_to_inertial_rotation_matrix, rsw_to_inertial_rotation_matrix]])
-
-    #         total_inertial_to_rsw_rotation_matrix = np.linalg.inv(total_rsw_to_inertial_rotation_matrix)
-    #         if index == 0:
-    #             print(total_rsw_to_inertial_rotation_matrix[:3,:3], total_inertial_to_rsw_rotation_matrix[:3,:3])
-
-    #         # Convert J2000 states to Moon-fixed coordinates
-    #         state_history_lpf_moon_dim = np.dot(total_inertial_to_rsw_rotation_matrix, state_lpf[index]-state_history_moon[index, 1:])
-    #         state_history_lumio_moon_dim = np.dot(total_inertial_to_rsw_rotation_matrix, state_lumio[index]-state_history_moon[index, 1:])
-
-    #         # print(state_history_lpf_moon_dim, state_history_lumio_moon_dim)
-
-    #         # Convert Moon-fixed dimensional coordinates to non-dimensional form
-    #         self.lu_cr3bp = np.linalg.norm(state_history_moon[0, 1:4])
-    #         self.tu_cr3bp = 1/np.sqrt((self.gravitational_parameter_primary + self.gravitational_parameter_secondary)/self.lu_cr3bp**3)
-    #         state_history_lpf_moon_nondim = np.concatenate((state_history_lpf_moon_dim[0:3]/self.lu_cr3bp, state_history_lpf_moon_dim[3:6]/(self.lu_cr3bp/self.tu_cr3bp)))
-    #         state_history_lumio_moon_nondim = np.concatenate((state_history_lumio_moon_dim[0:3]/self.lu_cr3bp, state_history_lumio_moon_dim[3:6]/(self.lu_cr3bp/self.tu_cr3bp)))
-
-    #         # Convert Moon-centric non-dimensionalized state to barycentric states.
-    #         state_history_lpf_moon_nondim = state_history_lpf_moon_nondim + (1-self.mu)
-    #         state_history_lumio_moon_nondim = state_history_lumio_moon_nondim + (1-self.mu)
-
-    #         # print(state_history_lpf_moon_nondim, state_history_lumio_moon_nondim)
-
-    #         # Save state history
-    #         initial_states[index]  = np.concatenate((state_history_lpf_moon_nondim, state_history_lumio_moon_nondim))
-
-
-    #     ax = plt.figure().add_subplot(projection='3d')
-    #     plt.plot(initial_states[:,0], initial_states[:,1], initial_states[:,2])
-    #     plt.plot(initial_states[:,6], initial_states[:,7], initial_states[:,8])
-    #     plt.axis('equal')
-    #     # plt.plot(state_history_moon[:,1], state_history_moon[:,2], state_history_moon[:,3])
-    #     plt.show()
+        return self.initial_state
 
 
     def get_closest_initial_state(self):
@@ -231,7 +93,7 @@ class LowFidelityDynamicModel(DynamicModelBase):
         initial_state_history = np.empty((0, 12))
         for initial_state_barycenter_fixed in state_history_erdem:
 
-            self.convert_synodic_to_inertial_state(initial_state_barycenter_fixed)
+            self.initial_state = self.convert_synodic_to_inertial_state(initial_state_barycenter_fixed)
 
             distance = np.linalg.norm(self.initial_state[6:12]-state_history_polimi[0])
 
@@ -257,7 +119,8 @@ class LowFidelityDynamicModel(DynamicModelBase):
 
         # print(closest_initial_state)
 
-        # closest_state[6:12] = np.array([-3.09965465e+08,  3.07254364e+08,  1.09190626e+08, -6.94514593e+02, -5.92981679e+02, -3.02950273e+02])
+        # closest_initial_state[6:12] = np.array([-3.09965465e+08,  3.07254364e+08,  1.09190626e+08, -6.94514593e+02, -5.92981679e+02, -3.02950273e+02])
+        # print(closest_initial_state)
         return closest_initial_state
 
 
@@ -295,13 +158,6 @@ class LowFidelityDynamicModel(DynamicModelBase):
         # Update the body settings
         self.bodies = environment_setup.create_system_of_bodies(self.body_settings)
 
-        # print(self.bodies.get(self.name_primary).mass,\
-        #     self.bodies.get(self.name_secondary).mass)
-        # print(self.bodies.get(self.name_ELO).mass,\
-        #     self.bodies.get(self.name_LPO).mass)
-
-        # print(self.mu, constants.GRAVITATIONAL_CONSTANT)
-
 
     def set_acceleration_settings(self):
 
@@ -328,7 +184,12 @@ class LowFidelityDynamicModel(DynamicModelBase):
 
         self.set_acceleration_settings()
 
-        self.initial_state = self.get_closest_initial_state()
+        if self.custom_initial_state is None:
+            self.initial_state = self.get_closest_initial_state()
+        else:
+
+            self.initial_state = self.convert_synodic_to_inertial_state(self.custom_initial_state)
+            print("self.initial_state: ", self.initial_state)
 
 
     def set_integration_settings(self):
@@ -337,7 +198,7 @@ class LowFidelityDynamicModel(DynamicModelBase):
 
         current_coefficient_set = propagation_setup.integrator.CoefficientSets.rkdp_87
         current_tolerance = 1e-10*constants.JULIAN_DAY
-        initial_time_step = 1e-3*constants.JULIAN_DAY
+        initial_time_step = 1e-6*constants.JULIAN_DAY
         self.integrator_settings = propagation_setup.integrator.runge_kutta_variable_step_size(initial_time_step,
                                                                                         current_coefficient_set,
                                                                                         np.finfo(float).eps,
@@ -345,7 +206,8 @@ class LowFidelityDynamicModel(DynamicModelBase):
                                                                                         current_tolerance,
                                                                                         current_tolerance)
 
-        self.integrator_settings = propagation_setup.integrator.runge_kutta_4(self.simulation_start_epoch, 0.005*86400)
+        # self.integrator_settings = propagation_setup.integrator.runge_kutta_4(self.simulation_start_epoch, 0.005*constants.JULIAN_DAY)
+
 
     def set_dependent_variables_to_save(self):
 
@@ -353,13 +215,20 @@ class LowFidelityDynamicModel(DynamicModelBase):
 
         # Define required outputs
         self.dependent_variables_to_save = [
-            propagation_setup.dependent_variable.keplerian_state(self.name_secondary, self.name_primary),
-            propagation_setup.dependent_variable.relative_position(self.name_primary, self.name_secondary),
-            propagation_setup.dependent_variable.relative_velocity(self.name_primary, self.name_secondary),
-            propagation_setup.dependent_variable.relative_distance(self.name_primary, self.name_secondary),
+            propagation_setup.dependent_variable.relative_position(self.name_secondary, self.name_primary),
+            propagation_setup.dependent_variable.relative_velocity(self.name_secondary, self.name_primary),
+            propagation_setup.dependent_variable.relative_position(self.name_ELO, self.name_LPO),
+            propagation_setup.dependent_variable.relative_velocity(self.name_ELO, self.name_LPO),
             propagation_setup.dependent_variable.total_acceleration(self.name_ELO),
-            propagation_setup.dependent_variable.total_acceleration(self.name_LPO)
-        ]
+            propagation_setup.dependent_variable.total_acceleration(self.name_LPO)]
+
+        self.dependent_variables_to_save.extend([
+            propagation_setup.dependent_variable.single_acceleration_norm(
+                    propagation_setup.acceleration.point_mass_gravity_type, body_to_propagate, body_to_create) \
+                        for body_to_propagate in self.bodies_to_propagate for body_to_create in self.bodies_to_create])
+
+        self.dependent_variables_to_save.extend([propagation_setup.dependent_variable.body_mass(self.name_primary),
+                                                 propagation_setup.dependent_variable.body_mass(self.name_secondary)])
 
 
     def set_termination_settings(self):
@@ -409,8 +278,11 @@ class LowFidelityDynamicModel(DynamicModelBase):
 
 
 
-# test2 = LowFidelityDynamicModel(60390, 28)
-# states2 = test2.get_propagated_orbit()[0]
+# custom_initial_state = None
+# custom_initial_state = np.array([0.98512134, 0.00147649, 0.00492546, -0.87329730, -1.61190048, 0, \
+#                                  1.1473302, 0, -0.15142308, 0, -0.21994554, 0])
+# test2 = LowFidelityDynamicModel(60390, 28, custom_initial_state=custom_initial_state)
+# states2 = np.stack(list(test2.get_propagated_orbit()[0].state_history.values()))
 # # print(test2.convert_cartesian_state_to_synodic())
 
 # ax = plt.figure().add_subplot(projection='3d')
