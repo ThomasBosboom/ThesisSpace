7bf8b7cdde7215e7d610d6bdb1020149c66ec763
ThomasBosboom <thomasbosboom@hotmail.com>
2023-12-22 17:37:11 +0100

    Updates to tests and overall structure

--- simulations/tests/test_dynamic_models/test_dynamic_models.py
+++ simulations/tests/test_dynamic_models/test_dynamic_models.py
@@ -5,17 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define path to import src files
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+script_directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(script_directory)
 parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(parent_dir)
+
 
 # Third party
 import pytest
 import pytest_html
 from tudatpy.kernel import constants
-import mpld3
+from tudatpy.kernel.numerical_simulation import propagation_setup
+# import mpld3
 
 # Own
+from tests import utils
 from src.dynamic_models import validation_LUMIO
 from src.dynamic_models import Interpolator, FrameConverter
 from src.dynamic_models.low_fidelity.integration_settings import *
@@ -23,176 +27,154 @@ from src.dynamic_models.high_fidelity.point_mass import *
 from src.dynamic_models.high_fidelity.point_mass_srp import *
 from src.dynamic_models.high_fidelity.spherical_harmonics import *
 from src.dynamic_models.high_fidelity.spherical_harmonics_srp import *
-# from src.estimation_models import EstimationModel
-
+from src.estimation_models import EstimationModel
 
 
 class TestOutputsDynamicalModels:
 
     @pytest.mark.parametrize(
-    "first_value, second_value",
+    "simulation_start_epoch_MJD, propagation_time",
     [
-        (10, 8),
-        (8, 6)
+        (60390, 10),
     ])
 
-    def test_parametrize(self, first_value, second_value):
-        assert first_value - 2 == second_value
+    def test_estimation_models(self, simulation_start_epoch_MJD, propagation_time):
 
-# package_dict={"low_fidelity": ["integration_settings"], "high_fidelity": ["point_mass", "point_mass_srp"]}
-    def get_dynamic_model_objects(self, simulation_start_epoch_MJD, propagation_time, package_dict={"low_fidelity": ["integration_settings"], "high_fidelity": ["point_mass", "point_mass_srp"]}):
+        dynamic_model_objects = utils.get_dynamic_model_objects(simulation_start_epoch_MJD, propagation_time)
+        estimation_model_objects = utils.get_estimation_model_objects(EstimationModel, dynamic_model_objects)
 
-        # Get a list of all relevant packages within 'high_fidelity'
-        dynamic_model_objects = {}
-        for package_type, package_name_list in package_dict.items():
+        for package_type, package_names in estimation_model_objects.items():
+            for package_name, estimation_model_objects in package_names.items():
+                for estimation_model_object in estimation_model_objects:
 
-            sub_dict = {package_name_list[i]: [] for i in range(len(package_name_list))}
-            packages_dir = os.path.join(parent_dir, 'src', 'dynamic_models', package_type)
-
-            package_name_counter = 0
-            for package_name in package_dict[package_type]:
-
-                package_module_path = f'dynamic_models.{package_type}.{package_name}'
-                package_module = __import__(package_module_path, fromlist=[package_name])
-                package_files = os.listdir(os.path.join(packages_dir, package_name))
-
-                for file_name in package_files:
-
-                    if file_name.endswith('.py') and not file_name.startswith('__init__'):
-                        module_path = f'{package_module_path}.{os.path.splitext(file_name)[0]}'
-                        module = __import__(module_path, fromlist=[file_name])
-
-                        if package_type == "low_fidelity":
-                            DynamicModel = module.LowFidelityDynamicModel(simulation_start_epoch_MJD, propagation_time)
-                        else:
-                            DynamicModel = module.HighFidelityDynamicModel(simulation_start_epoch_MJD, propagation_time)
-
-                        sub_dict[package_name_list[package_name_counter]].extend([DynamicModel])
-                        dynamic_model_objects[package_type] = sub_dict
-
-                package_name_counter += 1
-
-        return dynamic_model_objects
+                    print(estimation_model_object.get_estimation_results()[0])
 
 
     @pytest.mark.parametrize(
     "simulation_start_epoch_MJD, propagation_time",
     [
-        (60390, 30),
-        # (60395, 1),
+        (60390, 10),
+        # (60395, 10),
+        # (60400, 10),
     ])
 
-    # def test_compare_dynamic_models(self, simulation_start_epoch_MJD, propagation_time, extras):
+    def test_compare_dynamic_models(self, simulation_start_epoch_MJD, propagation_time, extras):
 
-    #     dynamic_model_objects = self.get_dynamic_model_objects(simulation_start_epoch_MJD, propagation_time)
+        dynamic_model_objects = utils.get_dynamic_model_objects(simulation_start_epoch_MJD, propagation_time)
 
-    #     # Loop through each key in the outer dictionary
-    #     i = 0
-    #     for package_type, package_names in dynamic_model_objects.items():
-    #         for package_name, dynamic_models in package_names.items():
-    #             for dynamic_model in dynamic_models:
+        # Create a figure and three subplots side by side
+        fig, axs = plt.subplots(1, 3, figsize=(15, 5), sharex=True, sharey=True)
+        for package_type, package_names in dynamic_model_objects.items():
+            for package_name, dynamic_models in package_names.items():
+                for dynamic_model in dynamic_models:
 
-    #                 print(f"dynamic_model: {dynamic_model}")
+                    # Create a folder named after the function
+                    folder_name = "test_compare_dynamic_models"
+                    os.makedirs(folder_name, exist_ok=True)
 
-    #                 # Extract simulation histories numerical solution
-    #                 epochs, state_history, dependent_variables_history, state_transition_matrix_history = \
-    #                     Interpolator.Interpolator(dynamic_model, step_size=0.005*constants.JULIAN_DAY, epoch_in_MJD=False).get_results()
+                    setattr(dynamic_model, "current_coefficient_set", propagation_setup.integrator.CoefficientSets.rkf_89)
 
-    #                 # Extra simulation histories analytical solution
-    #                 state_history_barycentric_primary = dependent_variables_history[:,:6]*(-dynamic_model.mu)
-    #                 state_history_barycentric_secondary = dependent_variables_history[:,:6]*(1-dynamic_model.mu)
-    #                 state_history_barycentric = np.add(state_history[:,:6], state_history_barycentric_primary)
+                    # Extract simulation histories numerical solution
+                    epochs, state_history, dependent_variables_history, state_transition_matrix_history = \
+                        Interpolator.Interpolator(dynamic_model, step_size=0.005, epoch_in_MJD=False).get_results()
 
-    #                 print(epochs, np.shape(epochs))
-    #                 # print("state_history", state_history)
-    #                 # print("state_history_barycentric", state_history_barycentric)
-    #                 # print("difference: ", np.linalg.norm(state_history_barycentric_primary-state_history_barycentric_secondary, axis=1))
+                    # Define the titles for the subplots
+                    subplot_titles = ['Plot 1', 'Plot 2', 'Plot 3']
 
-    #                 # fig = plt.figure()
-    #                 # ax = fig.add_subplot(1,1,1)
-    #                 # ax.plot([1,2,3], color="red")
-    #                 # ax.plot([2,3,2], color="green")
-    #                 # plt.title("figure")
+                    # Data to plot
+                    data_to_plot = [dependent_variables_history[:, 6:9]]
 
-    #                 # # Save the figure
-    #                 # figure_path = f"C:/Users/thoma/OneDrive/Documenten/GitHub/ThesisSpace/simulations/tests/test_dynamic_models/figure{i}.png"
-    #                 # fig.savefig(figure_path)
+                    # Iterate through subplots and data
+                    for i, ax in enumerate(axs):
+                        for data in data_to_plot:
+                            ax.plot(data[:, i % 3], data[:, (i + 1) % 3], label=f"{dynamic_model}")
 
-    #                 # extras.append(pytest_html.extras.png(figure_path))
+                        ax.grid(alpha=0.5, linestyle='--')
+                        ax.set_title(subplot_titles[i])
+
+                    assert dynamic_model.name_ELO == "LPF"
+
+        plt.legend()
+        figure_path = os.path.join(folder_name, f"test_low_fidelity_dynamic_models_{simulation_start_epoch_MJD}_{propagation_time}.png")
+        fig.savefig(figure_path)
+        extras.append(pytest_html.extras.png(figure_path))
 
 
-    #                 i += 1
-
-    #         assert dynamic_model.name_ELO == "LPF"
-    #         assert dynamic_model.simulation_start_epoch_MJD == simulation_start_epoch_MJD
-
+    @pytest.mark.parametrize(
+    "simulation_start_epoch_MJD, propagation_time",
+    [
+        (60390, 10),
+        # (60395, 10),
+        # (60400, 10),
+    ])
 
     def test_low_fidelity_dynamic_models(self, simulation_start_epoch_MJD, propagation_time, extras):
 
-        custom_initial_state = np.array([0.985121349979458, 0.001476496155141, 0.004925468520363, -0.873297306080392, -1.611900486933861, 0, \
+        custom_initial_state = np.array([0.985141349979458, 0.001476496155141, 0.004925468520363, -0.873297306080392, -1.611900486933861, 0, \
                                          1.1473302, 0, -0.15142308, 0, -0.21994554, 0])
 
         # Generate LowFidelityDynamicModel object only
         dynamic_model = low_fidelity.LowFidelityDynamicModel(simulation_start_epoch_MJD, propagation_time, custom_initial_state=custom_initial_state)
 
         # Define step size that both tudatpy and classic model work run (variable step size epochs are adjusted in Interpolator)
-        step_size = 0.001
+        step_size = 0.005
 
         # Extract simulation histories tudatpy solution
         epochs, state_history, dependent_variables_history, state_transition_matrix_history = \
             Interpolator.Interpolator(dynamic_model, step_size=step_size).get_results()
 
         # Extract simulation histories classical solution
+        epochs_classic, state_history_classic, dependent_variables_history_classic = \
+            FrameConverter.SynodicToInertialHistoryConverter(dynamic_model, step_size=step_size).get_results()
 
-        custom_initial_state = np.array([0.985121349979458, 0.001476496155141, 0.004925468520363, -0.873297306080392, -1.611900486933861,  0, \
-                                         1.1473302, 0, -0.15142308, 0, -0.21994554, 0])
-
-        epochs_classic, state_history_classic, state_history_primaries = \
-            FrameConverter.SynodicToInertialHistoryConverter(dynamic_model, step_size=step_size).get_results(custom_initial_state=custom_initial_state)
-
-
-        print(epochs, np.shape(epochs))
-        print(epochs_classic, np.shape(epochs_classic))
-        print("tudatpy: ", state_history[0], "classic: ", state_history_classic[0])
-
-        ax = plt.figure()
+        fig1, ax = plt.subplots()
         plt.plot(epochs, np.abs(state_history[:,0]-state_history_classic[:,0]), label="LPF x")
         plt.plot(epochs, np.abs(state_history[:,1]-state_history_classic[:,1]), label="LPF y")
         plt.plot(epochs, np.abs(state_history[:,2]-state_history_classic[:,2]),  label="LPF z")
-
-        plt.plot(epochs, np.linalg.norm(state_history[:,:3]-state_history_classic[:,:3], axis=1), label="LPF")
-        plt.plot(epochs, np.linalg.norm(state_history[:,6:9]-state_history_classic[:,6:9], axis=1), label="LUMIO")
-        # plt.plot(epochs, np.linalg.norm(dependent_variables_history[:,:3]-state_history_primaries[:,:3], axis=1), label="Earth")
-        plt.plot(epochs, np.linalg.norm(dependent_variables_history[:,:3]-state_history_primaries[:,6:9], axis=1), label="Moon")
         plt.xlabel("Epoch")
         plt.ylabel("Difference [m]")
         plt.title("Absolute position difference tudatpy versus classical CRTBP")
         ax.legend()
         plt.grid(alpha=0.5)
         plt.yscale("log")
-        plt.show()
+        # plt.show()
 
-        ax = plt.figure()
+        fig2, ax = plt.subplots()
+        plt.plot(epochs, np.linalg.norm(state_history[:,0:3]-state_history_classic[:,0:3], axis=1), label="LPF w.r.t. Earth")
+        plt.plot(epochs, np.linalg.norm(state_history[:,6:9]-state_history_classic[:,6:9], axis=1), label="LUMIO w.r.t. Earth")
+        plt.plot(epochs, np.linalg.norm((state_history[:,0:3]-dependent_variables_history[:,0:3])-(state_history_classic[:,0:3]-dependent_variables_history_classic[:,6:9]), axis=1), label="LPF w.r.t. Moon")
+        plt.plot(epochs, np.linalg.norm((state_history[:,6:9]-dependent_variables_history[:,0:3])-(state_history_classic[:,6:9]-dependent_variables_history_classic[:,6:9]), axis=1), label="LUMIO w.r.t. Moon")
+        plt.plot(epochs, np.linalg.norm(np.zeros(np.shape(dependent_variables_history[:,0:3]))-dependent_variables_history_classic[:,0:3], axis=1), label="Earth w.r.t. Earth")
+        plt.plot(epochs, np.linalg.norm(dependent_variables_history[:,0:3]-dependent_variables_history_classic[:,6:9], axis=1), label="Moon w.r.t. Earth")
+        plt.xlabel("Epoch")
+        plt.ylabel("Difference [m]")
+        plt.title("Absolute position difference tudatpy versus classical CRTBP")
+        ax.legend()
+        plt.grid(alpha=0.5)
+        plt.yscale("log")
+        # plt.show()
+
+        fig3, ax = plt.subplots()
         plt.plot(epochs, np.linalg.norm(state_history[:,3:6]-state_history_classic[:,3:6], axis=1), label="LPF")
         plt.plot(epochs, np.linalg.norm(state_history[:,9:12]-state_history_classic[:,9:12], axis=1), label="LUMIO")
-        # plt.plot(epochs, np.linalg.norm(dependent_variables_history[:,3:6]-state_history_primaries[:,3:6], axis=1), label="Earth")
-        plt.plot(epochs, np.linalg.norm(dependent_variables_history[:,3:6]-state_history_primaries[:,9:12], axis=1), label="Moon")
+        plt.plot(epochs, np.linalg.norm((state_history[:,3:6]-dependent_variables_history[:,3:6])-(state_history_classic[:,3:6]-dependent_variables_history_classic[:,9:12]), axis=1), label="LPF w.r.t. Moon")
+        plt.plot(epochs, np.linalg.norm((state_history[:,9:12]-dependent_variables_history[:,3:6])-(state_history_classic[:,9:12]-dependent_variables_history_classic[:,9:12]), axis=1), label="LUMIO w.r.t. Moon")
+        plt.plot(epochs, np.linalg.norm(dependent_variables_history[:,3:6]-dependent_variables_history_classic[:,9:12], axis=1), label="Moon w.r.t. Earth")
         plt.xlabel("Epoch")
         plt.ylabel("Difference [m/s]")
         plt.title("Absolute velocity difference tudatpy versus classical CRTBP")
         ax.legend()
         plt.grid(alpha=0.5)
         plt.yscale("log")
-        plt.show()
+        # plt.show()
 
         ax = plt.figure().add_subplot(projection='3d')
-        # plt.plot(dependent_variables_history[:,0], dependent_variables_history[:,1], dependent_variables_history[:,2], label="moon w.r.t earth")
         plt.plot(state_history_classic[:,0], state_history_classic[:,1], state_history_classic[:,2], label="lpf classic")
-        plt.plot(state_history[:,0], state_history[:,1], state_history[:,2], label="lpf tudatpy")
         plt.plot(state_history_classic[:,6], state_history_classic[:,7], state_history_classic[:,8], label="lumio classic")
+        plt.plot(state_history[:,0], state_history[:,1], state_history[:,2], label="lpf tudatpy")
         plt.plot(state_history[:,6], state_history[:,7], state_history[:,8], label="lumio tudatpy")
-        plt.plot(state_history_primaries[:,0], state_history_primaries[:,1], state_history_primaries[:,2], label="earth classic")
-        plt.plot(state_history_primaries[:,6], state_history_primaries[:,7], state_history_primaries[:,8], label="moon classic")
+        plt.plot(dependent_variables_history_classic[:,0], dependent_variables_history_classic[:,1], dependent_variables_history_classic[:,2], label="earth classic")
+        plt.plot(dependent_variables_history_classic[:,6], dependent_variables_history_classic[:,7], dependent_variables_history_classic[:,8], label="moon classic")
         plt.plot(dependent_variables_history[:,0], dependent_variables_history[:,1], dependent_variables_history[:,2], label="moon tudatpy")
         ax.set_xlabel("X [km]")
         ax.set_ylabel("Y [km]")
@@ -200,12 +182,11 @@ class TestOutputsDynamicalModels:
         plt.title("Trajectories in inertial Earth-centered J2000 frame")
         ax.legend()
         plt.axis('equal')
-        plt.show()
-
+        # plt.show()
 
 
         # Create a figure and three subplots side by side
-        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+        fig4, axs = plt.subplots(1, 3, figsize=(15, 5))
 
         # Plot on the first subplot
         axs[0].plot(state_history[:,0], state_history[:,1])
@@ -226,13 +207,13 @@ class TestOutputsDynamicalModels:
         axs[2].set_title('Plot 3')
 
         plt.tight_layout()
-        plt.show()
+        # plt.show()
 
         # Save the figure
-        figure_path = f"C:/Users/thoma/OneDrive/Documenten/GitHub/ThesisSpace/simulations/tests/test_dynamic_models/comparison.png"
-        fig.savefig(figure_path)
-
-        extras.append(pytest_html.extras.png(figure_path))
+        for i, fig in enumerate([fig1, fig2, fig3, fig4]):
+            figure_path = os.path.join(script_directory, f"test_low_fidelity_dynamic_models_{simulation_start_epoch_MJD}_{propagation_time}_{i}.png")
+            fig.savefig(figure_path)
+            extras.append(pytest_html.extras.png(figure_path))
 
 
 
@@ -263,27 +244,12 @@ class TestOutputsDynamicalModels:
 
 
 
-# test = TestOutputsDynamicalModels()
-# dynamic_model_objects = test.get_dynamic_model_objects(60390, 10)
-# model = dynamic_model_objects["high_fidelity"]["point_mass"][0]
+print(os.getcwd())
+print(os.path.dirname(os.path.realpath(__file__)))
 
-# dynamics_simulator, variational_equations_solver = model.get_propagated_orbit()
-# print(model)
 
-# print(Interpolator.Interpolator(model).get_results()[0], np.shape(Interpolator.Interpolator(model).get_results()[0]))
-# # print(Interpolator.Interpolator(model).get_results()[1], np.shape(Interpolator.Interpolator(model).get_results()[1]))
-# # print(Interpolator.Interpolator(model).get_results()[2], np.shape(Interpolator.Interpolator(model).get_results()[2]))
-# # print(Interpolator.Interpolator(model).get_results()[3], np.shape(Interpolator.Interpolator(model).get_results()[3]))
+DynamicModel = high_fidelity_point_mass_01.HighFidelityDynamicModel(60390, 10)
 
-# state_history = Interpolator.Interpolator(model).get_results()[1]
-# ax = plt.figure().add_subplot(projection='3d')
-# ax.plot(state_history[:,0], state_history[:,1], state_history[:,2], color="red", label="LPF")
-# ax.plot(state_history[:,6], state_history[:,7], state_history[:,8], color="blue", label="LUMIO")
-# ax.set_xlabel("X [m]")
-# ax.set_ylabel("Y [m]")
-# ax.set_zlabel("Z [m]")
-# plt.axis('equal')
-# plt.legend()
-# plt.grid(alpha=0.2)
-# plt.tight_layout()
-# plt.show()
+print(DynamicModel.current_coefficient_set)
+setattr(DynamicModel, "current_coefficient_set", "CoefficientSets.rkf_89")
+print(DynamicModel.current_coefficient_set)
