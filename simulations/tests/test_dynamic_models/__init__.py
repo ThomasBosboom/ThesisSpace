dbeb0e4a0c1e96e203f8ae6210110013b422ad50
ThomasBosboom <thomasbosboom@hotmail.com>
2024-01-07 02:08:50 +0100

    Update __init__.py

--- simulations/tests/test_dynamic_models/__init__.py
+++ simulations/tests/test_dynamic_models/__init__.py
@@ -1,11 +1,4 @@
 import os
 import sys
 
-print(os.path.realpath(__file__))
-
-print(os.path.dirname(os.path.realpath(__file__)))
-
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-# print(sys.path)
-
