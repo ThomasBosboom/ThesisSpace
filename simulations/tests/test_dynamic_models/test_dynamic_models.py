70153098851e46aadfec3f94a43e93391ad554c4
ThomasBosboom <thomasbosboom@hotmail.com>
2024-01-10 20:30:49 +0100

    Update test_dynamic_models.py

--- simulations/tests/test_dynamic_models/test_dynamic_models.py
+++ simulations/tests/test_dynamic_models/test_dynamic_models.py
@@ -29,7 +29,7 @@ from src.dynamic_models.high_fidelity.point_mass import *
 from src.dynamic_models.high_fidelity.point_mass_srp import *
 from src.dynamic_models.high_fidelity.spherical_harmonics import *
 from src.dynamic_models.high_fidelity.spherical_harmonics_srp import *
-from src.estimation_models import EstimationModel
+from src.estimation_models import estimation_model
 
 
 class TestFrameConversions:
@@ -737,7 +737,7 @@ class TestOutputsDynamicModels:
         custom_initial_state = np.array([0.985121349979458, 0.001476496155141, 0.004925468520363, -0.873297306080392, -1.611900486933861, 0,	\
                                         1.147342501,	-0.0002324517381, -0.151368318,	-0.000202046355,	-0.2199137166,	0.0002817105509])
         dynamic_model_objects["low_fidelity"]["three_body_problem"][0] = low_fidelity.LowFidelityDynamicModel(simulation_start_epoch_MJD, propagation_time, custom_initial_state=custom_initial_state)
-        estimation_model_objects = utils.get_estimation_model_objects(EstimationModel, dynamic_model_objects)
+        estimation_model_objects = utils.get_estimation_model_objects(estimation_model, dynamic_model_objects)
 
         for estimation_model in utils.convert_model_objects_to_list(estimation_model_objects):
 
