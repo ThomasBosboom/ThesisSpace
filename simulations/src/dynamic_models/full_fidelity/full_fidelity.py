a2f7f337a01de8de8b561ac5443dad3447d96b8b
ThomasBosboom <thomasbosboom@hotmail.com>
2024-01-15 16:54:51 +0100

    Update full_fidelity.py

--- simulations/src/dynamic_models/full_fidelity/full_fidelity.py
+++ simulations/src/dynamic_models/full_fidelity/full_fidelity.py
@@ -98,17 +98,6 @@ class HighFidelityDynamicModel(DynamicModelBase):
             self.body_settings.get(body).radiation_source_settings = environment_setup.radiation_pressure.panelled_extended_radiation_source(
                 surface_radiosity_models, [6, 12, 18])
 
-        # tide_raising_body = "Earth"
-        # love_numbers = dict( )
-        # love_numbers[ 2 ] = list( )
-        # love_numbers[ 2 ].append( 0.024615 )
-        # love_numbers[ 2 ].append( 0.023915 )
-        # love_numbers[ 2 ].append( 0.024852 )
-        # gravity_field_variation_list = list()
-        # gravity_field_variation_list.append( environment_setup.gravity_field_variation.solid_body_tide_degree_order_variable_k(
-        #     tide_raising_body, love_numbers ))
-        # self.body_settings.get( "Earth" ).gravity_field_variation_settings = gravity_field_variation_list
-
         # Create environment
         self.bodies = environment_setup.create_system_of_bodies(self.body_settings)
 
@@ -157,43 +146,35 @@ class HighFidelityDynamicModel(DynamicModelBase):
 
         self.set_acceleration_settings()
 
-        # initial_state_LPF = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_ELO)
-        # initial_state_LUMIO = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_LPO)
-
         if self.custom_initial_state is not None:
             self.initial_state = self.custom_initial_state
+
         else:
-            # self.initial_state = np.concatenate((initial_state_LPF, initial_state_LUMIO))
-            self.initial_state = np.array([-2.81274203e+08,  2.51467494e+08,  1.46454277e+08, -1.16895572e+03,\
-                                            -2.16736162e+03, -7.88307987e+02, -3.10537998e+08,  2.49423157e+08,\
-                                            1.74937757e+08, -9.93171842e+02, -7.66408514e+02, -5.25173280e+02])
 
-        # Define the initial state of LPF
-        moon_initial_state = spice.get_body_cartesian_state_at_epoch(
-            target_body_name = self.name_secondary,
-            observer_body_name = self.name_primary,
-            reference_frame_name = self.global_frame_orientation,
-            aberration_corrections = 'NONE',
-            ephemeris_time = self.simulation_start_epoch)
+            # Define the initial state of LPF
+            moon_initial_state = spice.get_body_cartesian_state_at_epoch(
+                target_body_name = self.name_secondary,
+                observer_body_name = self.name_primary,
+                reference_frame_name = self.global_frame_orientation,
+                aberration_corrections = 'NONE',
+                ephemeris_time = self.simulation_start_epoch)
 
-        initial_state_lpf_moon = element_conversion.keplerian_to_cartesian_elementwise(
-            gravitational_parameter=self.gravitational_parameter_secondary,
-            semi_major_axis=5737.4E3,
-            eccentricity=0.61,
-            inclination=np.deg2rad(57.83),
-            argument_of_periapsis=np.deg2rad(90),
-            longitude_of_ascending_node=np.deg2rad(61.55),
-            true_anomaly=np.deg2rad(0))
+            initial_state_lpf_moon = element_conversion.keplerian_to_cartesian_elementwise(
+                gravitational_parameter=self.gravitational_parameter_secondary,
+                semi_major_axis=5737.4E3,
+                eccentricity=0.61,
+                inclination=np.deg2rad(57.83),
+                argument_of_periapsis=np.deg2rad(90),
+                longitude_of_ascending_node=np.deg2rad(61.55),
+                true_anomaly=np.deg2rad(0))
 
-        initial_state_LPF = np.add(initial_state_lpf_moon, moon_initial_state)
+            initial_state_LPF = np.add(initial_state_lpf_moon, moon_initial_state)
 
-        # Define the initial state of LUMIO
-        initial_state_LUMIO = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_LPO)
+            # Define the initial state of LUMIO
+            initial_state_LUMIO = validation_LUMIO.get_reference_state_history(self.simulation_start_epoch_MJD, self.propagation_time, satellite=self.name_LPO)
 
-        # Combine the initial states
-        self.initial_state = np.concatenate((initial_state_LPF, initial_state_LUMIO))
-        print("self.initial_state: ", self.initial_state)
-
+            # Combine the initial states
+            self.initial_state = np.concatenate((initial_state_LPF, initial_state_LUMIO))
 
 
     def set_integration_settings(self):
@@ -298,26 +279,26 @@ class HighFidelityDynamicModel(DynamicModelBase):
         return dynamics_simulator
 
 
-test = HighFidelityDynamicModel(60390, 365)
-dynamics_simulator = test.get_propagated_orbit()
+# test = HighFidelityDynamicModel(60390, 365)
+# dynamics_simulator = test.get_propagated_orbit()
 
-state_history = np.array([(time_conversion.julian_day_to_modified_julian_day(time_conversion.seconds_since_epoch_to_julian_day(key)), key, *value/1000) for key, value in dynamics_simulator.state_history.items()], dtype=object)
-moon_state_history = np.array([(time_conversion.julian_day_to_modified_julian_day(time_conversion.seconds_since_epoch_to_julian_day(key)), key, *value/1000) for key, value in dynamics_simulator.dependent_variable_history.items()], dtype=object)
+# state_history = np.array([(time_conversion.julian_day_to_modified_julian_day(time_conversion.seconds_since_epoch_to_julian_day(key)), key, *value/1000) for key, value in dynamics_simulator.state_history.items()], dtype=object)
+# moon_state_history = np.array([(time_conversion.julian_day_to_modified_julian_day(time_conversion.seconds_since_epoch_to_julian_day(key)), key, *value/1000) for key, value in dynamics_simulator.dependent_variable_history.items()], dtype=object)
 
-header = "epoch (MJD), epoch (seconds TDB), x [km], y [km], z [km], vx [km/s], vy [km/s], vz [km/s]"
-np.savetxt("C:/Users/thoma/OneDrive/Documenten/GitHub/ThesisSpace/simulations/reference/DataLPF/TextFiles/LPF_states_J2000_Earth_centered.txt", state_history[:,:8], delimiter=',', fmt='%f', header=header)
-np.savetxt("C:/Users/thoma/OneDrive/Documenten/GitHub/ThesisSpace/simulations/reference/DataLPF/TextFiles/Moon_states_J2000_Earth_centered.txt", moon_state_history[:,:8], delimiter=',', fmt='%f', header=header)
+# header = "epoch (MJD), epoch (seconds TDB), x [km], y [km], z [km], vx [km/s], vy [km/s], vz [km/s]"
+# np.savetxt("C:/Users/thoma/OneDrive/Documenten/GitHub/ThesisSpace/simulations/reference/DataLPF/TextFiles/LPF_states_J2000_Earth_centered.txt", state_history[:,:8], delimiter=',', fmt='%f', header=header)
+# np.savetxt("C:/Users/thoma/OneDrive/Documenten/GitHub/ThesisSpace/simulations/reference/DataLPF/TextFiles/Moon_states_J2000_Earth_centered.txt", moon_state_history[:,:8], delimiter=',', fmt='%f', header=header)
 
 # header = "epoch (MJD), epoch (seconds TDB), x [km], y [km], z [km], vx [km/s], vy [km/s], vz [km/s]"
 # np.savetxt("C:/Users/thoma/OneDrive/Documenten/GitHub/ThesisSpace/simulations/reference/DataLPF/TextFiles/LPF_states_J2000_Earth_centered_test.txt", state_history[:,:8], delimiter=',', fmt='%f', header=header)
 # np.savetxt("C:/Users/thoma/OneDrive/Documenten/GitHub/ThesisSpace/simulations/reference/DataLPF/TextFiles/Moon_states_J2000_Earth_centered_test.txt", moon_state_history[:,:8], delimiter=',', fmt='%f', header=header)
 
 
-ax = plt.figure().add_subplot(projection='3d')
-plt.plot(moon_state_history[:,2], moon_state_history[:,3], moon_state_history[:,4])
-plt.plot(state_history[:,2], state_history[:,3], state_history[:,4])
-plt.legend()
-plt.show()
+# ax = plt.figure().add_subplot(projection='3d')
+# plt.plot(moon_state_history[:,2], moon_state_history[:,3], moon_state_history[:,4])
+# plt.plot(state_history[:,2], state_history[:,3], state_history[:,4])
+# plt.legend()
+# plt.show()
 
 # dependent_variables_history = np.vstack(list(dynamics_simulator.dependent_variable_history.values()))
 # print(np.shape(dependent_variables_history))
