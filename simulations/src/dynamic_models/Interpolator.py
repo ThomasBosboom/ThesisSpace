cc373b0450dd4a52807383ee9500b863c89b90fa
ThomasBosboom <thomasbosboom@hotmail.com>
2024-01-31 21:58:14 +0100

    Update Interpolator.py

--- simulations/src/dynamic_models/Interpolator.py
+++ simulations/src/dynamic_models/Interpolator.py
@@ -26,35 +26,46 @@ class Interpolator:
             return interpolated_history
 
 
-    def get_propagation_results(self, dynamic_model_object, estimated_parameter_vector=None):
+    def get_propagation_results(self, dynamic_model_object, estimated_parameter_vector=None, solve_variational_equations=True):
 
         # Get simulation results from each dynamic model
-        if estimated_parameter_vector is not None:
-            self.dynamics_simulator, self.variational_equations_solver = dynamic_model_object.get_propagation_simulator(estimated_parameter_vector=estimated_parameter_vector)
+        # if estimated_parameter_vector is not None:
+        if solve_variational_equations:
+            self.dynamics_simulator, self.variational_equations_solver = dynamic_model_object.get_propagation_simulator(estimated_parameter_vector=estimated_parameter_vector,
+                                                                                                                        solve_variational_equations=solve_variational_equations)
         else:
-            self.dynamics_simulator, self.variational_equations_solver = dynamic_model_object.get_propagation_simulator()
+            self.dynamics_simulator = dynamic_model_object.get_propagation_simulator(estimated_parameter_vector=estimated_parameter_vector,
+                                                                                     solve_variational_equations=solve_variational_equations)
+        # else:
+        #     self.dynamics_simulator, self.variational_equations_solver = dynamic_model_object.get_propagation_simulator(solve_variational_equations=solve_variational_equations)
         self.simulation_start_epoch = dynamic_model_object.simulation_start_epoch
         self.simulation_end_epoch = dynamic_model_object.simulation_end_epoch
-
-        # Extract the variational_equations_solver results
-        epochs                          = np.stack(list(self.variational_equations_solver.state_history.keys()))
-        state_history                   = np.stack(list(self.variational_equations_solver.state_history.values()))
-        state_transition_matrix_history = np.stack(list(self.variational_equations_solver.state_transition_matrix_history.values()))
 
         # Define updated time vector that is the same for all dynamic models irrespective of their own time vector
         interp_epochs = np.arange(self.simulation_start_epoch, self.simulation_end_epoch+self.step_size, self.step_size)
 
-        # Perform interpolation using on the results from self.variational_equations_solver
-        interp_state_history = self.interp_function(epochs, interp_epochs, state_history)
-        interp_state_transition_matrix_history = self.interp_function(epochs, interp_epochs, state_transition_matrix_history)
+        if solve_variational_equations:
+             # Extract the variational_equations_solver results
+            epochs                          = np.stack(list(self.variational_equations_solver.state_transition_matrix_history.keys()))
+            state_transition_matrix_history = np.stack(list(self.variational_equations_solver.state_transition_matrix_history.values()))
+
+            # Perform interpolation
+            interp_state_transition_matrix_history = self.interp_function(epochs, interp_epochs, state_transition_matrix_history)
 
         # Perform interpolation using on the results from dynamics_simulator
-        epochs                          = np.stack(list(self.dynamics_simulator.dependent_variable_history.keys()))
+        epochs                          = np.stack(list(self.dynamics_simulator.state_history.keys()))
+        state_history                   = np.stack(list(self.dynamics_simulator.state_history.values()))
         dependent_variables_history     = np.stack(list(self.dynamics_simulator.dependent_variable_history.values()))
+
+        # Perform interpolation
+        interp_state_history = self.interp_function(epochs, interp_epochs, state_history)
         interp_dependent_variables_history = self.interp_function(epochs, interp_epochs, dependent_variables_history)
 
         if self.epoch_in_MJD:
             interp_epochs = np.array([time_conversion.julian_day_to_modified_julian_day(\
                 time_conversion.seconds_since_epoch_to_julian_day(interp_epoch)) for interp_epoch in interp_epochs])
 
-        return interp_epochs, interp_state_history, interp_dependent_variables_history, interp_state_transition_matrix_history
+        if solve_variational_equations:
+            return interp_epochs, interp_state_history, interp_dependent_variables_history, interp_state_transition_matrix_history
+        else:
+            return interp_epochs, interp_state_history, interp_dependent_variables_history
