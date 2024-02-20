96787481957a0a8910e39acc3da011fa7c2ca132
ThomasBosboom <thomasbosboom@hotmail.com>
2024-02-21 00:10:04 +0100

    Update StationKeeping.py
    Includes n-point TPM

--- simulations/src/dynamic_models/StationKeeping.py
+++ simulations/src/dynamic_models/StationKeeping.py
@@ -4,8 +4,6 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Third party
-
 # Define path to import src files
 script_directory = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(script_directory)
@@ -23,29 +21,25 @@ class StationKeeping:
     def __init__(self, dynamic_model_object, custom_initial_state=None, custom_propagation_time=14, step_size=0.001):
 
         self.dynamic_model_object = dynamic_model_object
-        self.dynamic_model_object.propagation_time = custom_propagation_time
-        self.simulation_start_epoch = self.dynamic_model_object.simulation_start_epoch
-        self.simulation_end_epoch = self.dynamic_model_object.simulation_end_epoch
-        self.propagation_time = self.dynamic_model_object.propagation_time
-
-        self.custom_initial_state = custom_initial_state
-        self.custom_propagation_time = custom_propagation_time
+        self.dynamic_model_object.custom_initial_state = custom_initial_state
+        self.dynamic_model_object.custom_propagation_time = custom_propagation_time
         self.step_size = step_size
 
 
-    def get_corrected_state_vector(self, correction_epoch, target_point_epoch, cut_off_epoch=0):
+    def get_corrected_state_vector(self, correction_epoch, target_point_epochs, cut_off_epoch=0):
 
         # Propagate the results of the dynamic model to generate target points
+        print("custom_initial_state at the start:", self.dynamic_model_object.custom_initial_state)
         epochs, state_history, dependent_variables_history, state_transition_matrix_history = \
             Interpolator.Interpolator(epoch_in_MJD=False, step_size=self.step_size).get_propagation_results(self.dynamic_model_object,
-                                                                                                            custom_initial_state=self.custom_initial_state,
-                                                                                                            custom_propagation_time=self.custom_propagation_time)
+                                                                                                            custom_initial_state=self.dynamic_model_object.custom_initial_state,
+                                                                                                            custom_propagation_time=self.dynamic_model_object.custom_propagation_time)
 
         # Get the reference orbit states
         reference_state_history = list()
         for body in self.dynamic_model_object.bodies_to_propagate:
             reference_state_history.append(validation.get_reference_state_history(self.dynamic_model_object.simulation_start_epoch_MJD,
-                                                                                    self.propagation_time,
+                                                                                    self.dynamic_model_object.custom_propagation_time,
                                                                                     satellite=body,
                                                                                     step_size=self.step_size,
                                                                                     get_full_history=True))
@@ -55,79 +49,89 @@ class StationKeeping:
         # Perform target point method algorithm
         state_deviation_history = state_history - reference_state_history
 
-        print(np.shape(state_deviation_history))
+        # print(np.shape(state_deviation_history))
         # fig = plt.figure()
-        # # print(np.shape(state_deviation_history))
-        # # plt.plot(state_deviation_history[:,0:3])
+        # plt.plot(state_deviation_history[:,0:3])
         # plt.plot(state_deviation_history[:,6:9])
-        # # plt.plot(state_history[:,6:9])
-        # # plt.plot(reference_state_history[:,6:9])
+        # plt.plot(state_history[:,6:9])
+        # plt.plot(reference_state_history[:,6:9])
         # plt.show()
 
-        i_tc = int(cut_off_epoch/self.step_size)
-        i_tv = int(correction_epoch/self.step_size)
-        i_ti = int(target_point_epoch/self.step_size)
-        dr_tc = state_deviation_history[i_tv,6:9]
-        dv_tc = state_deviation_history[i_tv,9:12]
-        dr_ti = state_deviation_history[i_ti,6:9]
-
+        R_i = 1e-2*np.eye(3)
+        Q = 1e-1*np.eye(3)
         Phi = state_transition_matrix_history
-        Phi_tcti = np.linalg.inv(Phi[i_tc]) @ Phi[i_ti]
-        Phi_tvti = np.linalg.inv(Phi[i_tv]) @ Phi[i_ti]
-        Phi_tvti_rr = Phi_tvti[6:9,6:9]
-        Phi_tvti_rv = Phi_tvti[6:9,9:12]
-        Phi_tcti_rr = Phi_tcti[6:9,6:9]
-        Phi_tcti_rv = Phi_tcti[6:9,9:12]
 
-        # R_i = 1e-2*np.eye(3)
-        # Q = 1e-1*np.eye(3)
+        final_sum = np.empty((3,))
+        total_sum = np.empty((3,3))
+        for target_point_epoch in target_point_epochs:
 
-        # A = -np.linalg.inv(np.add((Q.T+Q), Phi_tvti_rv.T @ (R_i.T + R_i) @ Phi_tvti_rv))
-        # alpha_i = Phi_tvti_rv.T @ (R_i.T + R_i) @ Phi_tcti_rr
-        # beta_i  = Phi_tvti_rv.T @ (R_i.T + R_i) @ Phi_tcti_rv
+            # Define the indexes
+            i_tc = int(cut_off_epoch/self.step_size)
+            i_tv = int(correction_epoch/self.step_size)
+            i_ti = int(target_point_epoch/self.step_size)
+            dr_tc = state_deviation_history[i_tc,6:9]
+            dv_tc = state_deviation_history[i_tc,9:12]
+            dr_ti = state_deviation_history[i_ti,6:9]
 
-        # delta_v = A @ (alpha_i @ dr_tc + beta_i @ dv_tc)
+            # Define the STMs at the right epochs
+            Phi_tcti = Phi[i_ti] @ np.linalg.inv(Phi[i_tc])
+            Phi_tvti = Phi[i_ti] @ np.linalg.inv(Phi[i_tv])
+            Phi_tvti_rr = Phi_tvti[6:9,6:9]
+            Phi_tvti_rv = Phi_tvti[6:9,9:12]
+            Phi_tcti_rr = Phi_tcti[6:9,6:9]
+            Phi_tcti_rv = Phi_tcti[6:9,9:12]
 
-        # print(A)
-        # print(alpha_i)
-        # print(beta_i)
-        # print("=======")
-        # print(dr_tc)
-        # print(alpha_i @ dr_tc)
-        # print(dv_tc)
-        # print(beta_i @ dv_tc)
-        # print(delta_v, np.linalg.norm(delta_v))
+            # Calculate the TPM equations using the STMs for a given target point
+            total_sum = np.add(total_sum, Phi_tvti_rv.T @ (R_i.T + R_i) @ Phi_tvti_rv)
 
-        delta_v = -np.linalg.inv(Phi_tvti_rv) @ Phi_tvti_rr @ dr_tc - dv_tc
+            alpha_i = Phi_tvti_rv.T @ (R_i.T + R_i) @ Phi_tcti_rr
+            beta_i  = Phi_tvti_rv.T @ (R_i.T + R_i) @ Phi_tcti_rv
+
+            final_sum = np.add(final_sum, alpha_i @ dr_tc + beta_i @ dv_tc)
+
+        A = -np.linalg.inv(np.add((Q.T+Q), total_sum))
+
+        delta_v = A @ final_sum
+
+        print("delta_v:", delta_v)
+
+        # delta_v = -np.linalg.inv(Phi_tvti_rv) @ Phi_tvti_rr @ dr_tc - dv_tc
+
+        # print("delta_v:", delta_v)
 
         state_history[i_tv, 9:12] += delta_v
 
-        return state_history[i_tv]
+        # return state_history[i_tv]
+        return delta_v
 
 
 
-# dynamic_model_objects = utils.get_dynamic_model_objects(60390,
+# dynamic_model_objects = utils.get_dynamic_model_objects(60391,
 #                                                         1,
 #                                                         package_dict=None,
 #                                                         get_only_first=False,
 #                                                         custom_initial_state=None)
 
 # dynamic_model_object = dynamic_model_objects["high_fidelity"]["point_mass"][0]
+# # dynamic_model_object = dynamic_model_objects["low_fidelity"]["three_body_problem"][0]
 
+# custom_initial_state = np.array([-3.34034638e+08,  1.91822560e+08,  1.11600187e+08, -1.22100520e+02,
+#                                  -7.02130739e+02, -9.74257591e+02, -3.83004013e+08,  1.80617292e+08,
+#                                   1.22243914e+08, -6.84094596e+02, -8.16779163e+02, -6.68497305e+02])
 
-# custom_initial_state = np.array([-2.80124757e+08,  2.53324773e+08,  1.46943725e+08, -1.61475000e+03,
-#  -2.23501900e+03, -2.97165000e+02, -3.10469279e+08,  2.49476176e+08,
-#   1.74974083e+08, -9.93405005e+02, -7.66336485e+02, -5.24990115e+02])
-# # custom_initial_state = np.array([-3.34034721e+08,  1.91822727e+08,  1.11599990e+08, -1.22117098e+02,
-# #  -7.02102813e+02, -9.74268692e+02, -3.83003556e+08,  1.80616085e+08,
-# #   1.22244565e+08, -6.84093312e+02, -8.16786564e+02, -6.68493579e+02])
-# station_keeping = StationKeeping(dynamic_model_object, custom_initial_state=custom_initial_state, custom_propagation_time=28, step_size=0.01)
+# import time
 
-# lists = [[0, 20]]
-# for list1 in lists:
+# # lists = [[7, [21]], [7, [21, 28]]]
+# lists = [[0, [21]], [0, [21, 28]]]
+# for i, list1 in enumerate(lists):
 #     print(list1)
-#     corrected_state_vector = station_keeping.get_corrected_state_vector(correction_epoch=list1[0], target_point_epoch=list1[1], cut_off_epoch=0)
-#     print("corrected_state_vector:", corrected_state_vector)
+#     start_time = time.time()
+#     station_keeping = StationKeeping(dynamic_model_object, custom_initial_state=custom_initial_state, custom_propagation_time=max(list1[1]), step_size=0.01)
+#     delta_v = station_keeping.get_corrected_state_vector(cut_off_epoch=list1[0], correction_epoch=list1[0], target_point_epochs=list1[1])
+#     # print("delta_v:", delta_v)
+#     lists[i].append(time.time()-start_time)
+# print(lists)
+# plt.show()
 
 
 
