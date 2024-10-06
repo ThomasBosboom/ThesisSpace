386fbc6ac83d3ffbcdd8464a01f8e3981060e147
ThomasBosboom <thomasbosboom@hotmail.com>
2024-10-06 18:01:19 +0200

    update

--- simulations/tests/analysis/navigation_analysis.py
+++ simulations/tests/analysis/navigation_analysis.py
@@ -28,7 +28,7 @@ duration = 28
 mission_start_epoch = 60390.0
 
 auxilary_settings = {
-    "step_size": 0.001,
+    "step_size": 0.01,
     # "apriori_covariance": np.diag(np.array([5e2, 5e2, 5e2, 1e-2, 1e-2, 1e-2, 5e2, 5e2, 5e2, 1e-2, 1e-2, 1e-2])**2)
     # "orbit_insertion_error": np.array([0, 0, 0, 0, 0, 0, 1e3, 1e3, 1e3, 1e-2, 1e-2, 1e-2])*1,
     "include_station_keeping": True,
@@ -139,8 +139,8 @@ observation_windows_settings = {
 #     for arc_interval in arc_intervals
 # }
 
-mission_start_epoch = 60390
-num_runs = 5
+# mission_start_epoch = 60390
+num_runs = 1
 observation_windows_settings = {
     f"Baseline": [
         (helper_functions.get_constant_arc_observation_windows(28, arc_interval=3.0, arc_duration=1.0), num_runs, None),
@@ -189,6 +189,17 @@ observation_windows_settings = {
     ],
 }
 
+
+duration = 365
+arc_lengths = [0.5]
+num_runs = 1
+observation_windows_settings = {
+    f"Timing LUMIO": [
+        (helper_functions.get_lumio_like_observation_windows(duration, pattern=[7, 7, 7, 14], arc_duration=arc_length, mission_start_epoch=60390), num_runs, None)
+    ]
+    for arc_length in arc_lengths
+}
+
 #######################################################
 ###### Generate the navigation outputs ################
 #######################################################
@@ -222,36 +233,37 @@ for type_index, (window_type, navigation_outputs_cases) in enumerate(navigation_
                             }
                         )
 
-                        # process_single_navigation_results.plot_estimation_error_history(plot3d=True)
-                        # process_single_navigation_results.plot_estimation_error_history(plot3d=False)
-                        # process_single_navigation_results.plot_uncertainty_history()
-                        # process_single_navigation_results.plot_dispersion_history(plot3d=True)
-                        # process_single_navigation_results.plot_dispersion_history(plot3d=False)
-                        # process_single_navigation_results.plot_full_state_history(show_trajectories_only=False)
-                        # process_single_navigation_results.plot_full_state_history(show_trajectories_only=True)
-                        # process_single_navigation_results.plot_formal_error_history()
-                        # process_single_navigation_results.plot_observations()
-                        # process_single_navigation_results.plot_correlation_history()
+                        process_single_navigation_results.plot_estimation_error_history(plot3d=True)
+                        process_single_navigation_results.plot_estimation_error_history(plot3d=False)
+                        process_single_navigation_results.plot_uncertainty_history()
+                        process_single_navigation_results.plot_dispersion_history(plot3d=True)
+                        process_single_navigation_results.plot_dispersion_history(plot3d=False)
+                        process_single_navigation_results.plot_full_state_history(show_trajectories_only=False)
+                        process_single_navigation_results.plot_full_state_history(show_trajectories_only=True)
+                        process_single_navigation_results.plot_formal_error_history()
+                        process_single_navigation_results.plot_observations()
+                        process_single_navigation_results.plot_correlation_history()
                         # process_single_navigation_results.plot_observability_metrics()
 
 # plt.show()
 
 process_multiple_navigation_results = ProcessNavigationResults.PlotMultipleNavigationResults(
     navigation_outputs,
-    # color_cycle=["salmon", "forestgreen"],
+    color_cycle=["salmon", "forestgreen", "forestgreen", "forestgreen", "forestgreen", "forestgreen", "forestgreen"],
+    # color_cycle=["red", "green", "green", "green", "green", "green", "green"],
     figure_settings={"save_figure": True,
                     "current_time": current_time,
                     "file_name": file_name
     }
 )
 
-# process_multiple_navigation_results.plot_uncertainty_comparison()
-# process_multiple_navigation_results.plot_maneuvre_costs(separate_plots=False)
-# process_multiple_navigation_results.plot_maneuvre_costs(separate_plots=True, include_velocity=False)
-# process_multiple_navigation_results.plot_maneuvre_costs(separate_plots=True, include_velocity=True)
-# process_multiple_navigation_results.plot_full_state_history_comparison(step_size=None)
-# process_multiple_navigation_results.plot_monte_carlo_estimation_error_history(evaluation_threshold=14)
-# process_multiple_navigation_results.plot_estimation_arc_comparison(evaluation_threshold=14, bar_labeler=None,  worst_case=False)
+process_multiple_navigation_results.plot_uncertainty_comparison()
+process_multiple_navigation_results.plot_maneuvre_costs(separate_plots=False)
+process_multiple_navigation_results.plot_maneuvre_costs(separate_plots=True, include_velocity=False)
+process_multiple_navigation_results.plot_maneuvre_costs(separate_plots=True, include_velocity=True)
+process_multiple_navigation_results.plot_full_state_history_comparison(step_size=0.001)
+process_multiple_navigation_results.plot_monte_carlo_estimation_error_history(evaluation_threshold=14)
+process_multiple_navigation_results.plot_estimation_arc_comparison(evaluation_threshold=14, bar_labeler=None,  worst_case=False)
 process_multiple_navigation_results.plot_maneuvre_costs_bar_chart(evaluation_threshold=14, show_annual=False, bar_labeler=None, worst_case=False, observation_windows_settings=observation_windows_settings)
 process_multiple_navigation_results.plot_maneuvre_costs_bar_chart(evaluation_threshold=14, show_annual=True, bar_labeler=None, worst_case=False, observation_windows_settings=observation_windows_settings)
 process_multiple_navigation_results.plot_average_power_bar_chart(evaluation_threshold=14, show_annual=True, bar_labeler=None, worst_case=False, observation_windows_settings=observation_windows_settings)
